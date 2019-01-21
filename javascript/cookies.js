ca9cfd60184121499cf50004062f5af2e1566e60
Tobsii <sophie.tobisch@outlook.com>
2019-01-21 11:06:04 +0100

    Onsite Tracking with cookies

--- javascript/cookies.js
+++ javascript/cookies.js
@@ -1,3 +1,8 @@
-function myFunction(word){
-    console.log(word);
+function getTags(clickedElement){
+    var tagsInText = clickedElement.getElementsByClassName("tags")[0].innerHTML;
+    var seperateTagsFromText = tagsInText.split(":");
+    var tagsArray = seperateTagsFromText[1];
+    
+    console.log(tagsArray);
+    // TODO - ANALYZE & SAVE THEM? WHERE?
 }
