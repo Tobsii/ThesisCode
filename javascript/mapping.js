b618551a25e60731f73b5480a0ab8a9112c3064a
Tobsii <sophie.tobisch@outlook.com>
2019-01-30 16:41:25 +0100

    eigenes Profil in Variablen

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -2,11 +2,28 @@ import {createPage} from './main';
 
 export function parseAndMap(internProfile, externProfile, ownProfile, cookie){
         // intern Variablen
+        var ownAge
+        var ownCategories
+        var ownAuthor
+        var ownDownloads
       
         ownProfile.map(function(user){
             if (user.id == cookie){
-          
-            }
+                ownAuthor = user.favoriteAuthor
+                ownDownloads = user.downloads
+                var birthday = user.birthday.split(".")
+                ownAge = calculate_age(birthday[1], birthday[0], birthday[2])
+
+                // setze Kategorien String zusammen
+                for(var i = 0; i < user.favoriteFood.length; i++){
+                    if(i == user.favoriteFood.length-1){
+                    ownCategories+= user.favoriteFood[i]
+                    } else{
+                        ownCategories+= user.favoriteFood[i] + ","
+                    } 
+                }
+                console.log(ownAge, ownCategories, ownAuthor, ownDownloads)
+            } // end if
         })
         
         externProfile.map(function(user){
@@ -45,4 +62,23 @@ export function parseAndMap(internProfile, externProfile, ownProfile, cookie){
       
         createPage(allCategories, lastKlicked)
       
+}
+
+function calculate_age(birth_month,birth_day,birth_year)
+{
+    today_date = new Date();
+    today_year = today_date.getFullYear();
+    today_month = today_date.getMonth();
+    today_day = today_date.getDate();
+    age = today_year - birth_year;
+
+    if ( today_month < (birth_month - 1))
+    {
+        age--;
+    }
+    if (((birth_month - 1) == today_month) && (today_day < birth_day))
+    {
+        age--;
+    }
+    return age;
 }
